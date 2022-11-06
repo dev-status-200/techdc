d42c349799324eac61e6f1601549a137f28886ec
dev-status-200 <dev.status.200@gmail.com>
2022-11-06 13:48:47 +0500

    update

--- pages/index.js
+++ pages/index.js
@@ -21,20 +21,49 @@ export default function App() {
 
   const [i, setI] = useState(0);
   const [load, setLoad] = useState(true);
+  const [loadTwo, setLoadTwo] = useState(true);
+
+  const [width, setWidth] = useState(0);
+
+useEffect(() => {
+  setTimeout(() => {
+    setLoad(false)
+  }, 2000);
+}, [])
+
   useEffect(() => {
     aos.init({duration:500});
     setTimeout(() => {
       if (i === colors.length) setI(0);
       else setI(i + 1);
     }, 1000);
-    
+
     setTimeout(() => {
       setLoad(false)
     }, 2000);
 
   }, [i]);
+
+  useEffect(() => {
+      console.log(width)
+      window.addEventListener("resize", resize)
+      const timer = setTimeout(() => {
+        resize()
+      }, 1000);
+
+      return () => clearTimeout(timer)
+    }, [width])
+    
+    function resize(){
+      if (typeof window !== "undefined") {
+        setWidth(window.innerWidth)
+      }
+    }
+
   return (
     <div className="home-styles" style={{overflowX:'hidden', overflowY:'hidden'}} data-aos="flip-up">
+    {width>400 &&
+    <>
       {!load && <div className="bg" data-aos="fade-in">
       <Cursor hollow color={colors[i]} duration={0.8} size={45} />
       <Row style={{minHeight:'100vh'}}>
@@ -64,7 +93,29 @@ export default function App() {
           <img src='/loader.svg' height={100} />
         </div>
       }
+    </>
+    }
+    {width<400 &&
+      <>
+      {!load &&
+      <div className="bg" data-aos="fade-in" style={{backgroundColor:'#366caf'}}>
+      <Row style={{minHeight:'100vh', width:'100vw'}}>
+      </Row>
+        <div className="text-bg">
+        <img src="/logo.png" style={{height:100, width:100}} />
+          <div className="mobile-heading">UNDER</div>
+          <div className="mobile-heading">CONSTRUCTION</div>
+          <button className="btn-custom my-5">Contact Us</button>
+        </div>
+      </div>
+      }
+      {load && 
+        <div style={{ overflowX:'hidden', textAlign:'center', marginTop:'70%'}}>
+          <img src='/loader.svg' height={100}  />
+        </div>
+      }
+      </>
+    }
     </div>
-    
   );
 }
